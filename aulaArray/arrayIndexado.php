e3ddbdc1954b7b335ee7ab80ce51c0c204ecabea
Vinicius <viniciusr.camargo@gmail.com>
2025-05-04 22:24:11 -0300

    Exercícios ETEC

--- aulaArray/arrayIndexado.php
+++ aulaArray/arrayIndexado.php
@@ -1,6 +1,6 @@
 <?php
 
-$alunos = ["Gabriel", "Pedro", "Júlia", "Henrique", "Henrik", "Gustavo", "Kauã", "Davi", "Matheus", "Matheus", "Rômulo", "Geovana", "Felipe", "Vinicius"];
+$alunos = ["Gabriel", "Pedro", "Júlia", "Henrique", "Henrike", "Gustavo", "Kauã", "Davi", "Matheus", "Matheus", "Rômulo", "Geovana", "Felipe", "Vinicius"];
 //   inicio     fim        crescente ou decrescente
 for($cont =0; $cont < count($alunos); $cont++){
     echo $alunos[$cont].PHP_EOL;
